8ccf50472eda6c746eec4b2b86109909fa5165d1
Timur Ganiev <tim9624@gmail.com>
2021-09-27 14:22:12 +0300

    Move requirements to setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,6 @@
 from setuptools import setup
 
 
-def parse_requirements(path: str = 'requirements.txt'):
-    with open(path) as fp:
-        return fp.read().strip().split()
-
-
 setup(
     name='perceiver-io-pytorch',
     version='0.1.2',
@@ -16,5 +11,8 @@ setup(
     author='Timur Ganiev',
     author_email='ganiev.tmr@gmail.com',
     description='Unofficial Perceiver IO implementation',
-    install_requires=parse_requirements()
+    install_requires=[
+        'torch==1.9.0',
+        'einops==0.3.0'
+    ]
 )
