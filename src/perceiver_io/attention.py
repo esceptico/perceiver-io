c9d55b464d37d4905ea276656f6040597659fb90
Timur Ganiev <tim9624@gmail.com>
2021-09-27 23:57:09 +0300

    Add query/key and value output dim parameters

--- src/perceiver_io/attention.py
+++ src/perceiver_io/attention.py
@@ -12,7 +12,9 @@ class MultiHeadAttention(nn.Module):
         kv_dim: int,
         q_dim: int,
         *,
-        head_dim: Optional[int] = None,
+        qk_out_dim: Optional[int] = None,
+        v_out_dim: Optional[int] = None,
+        output_dim: Optional[int] = None,
         num_heads: int = 1,
         dropout: float = 0.0
     ):
@@ -21,26 +23,34 @@ class MultiHeadAttention(nn.Module):
         Args:
             kv_dim: Size of input key and value vectors.
             q_dim: Size of input query vector.
-            head_dim: Size of head. If None will be equal to
-                `q_dim / num_heads`. Defaults to None.
+            qk_out_dim: Size of Query and Key matrices last dimension.
+                If None, it will be equal to q_dim. Defaults to None.
+            v_out_dim: Size of Value matrix last dimension.
+                If None, it will be equal to qk_out_dim. Defaults to None.
+            output_dim: Size of output after the QKV attention.
+                If none, it will be equal to v_out_dim. Defaults to None.
             num_heads: Number of heads. Defaults to 1.
             dropout: Dropout probability. Defaults to 0.0.
         """
         super().__init__()
-        hidden_dim = q_dim
+
+        if qk_out_dim is None:
+            qk_out_dim = q_dim
+        if v_out_dim is None:
+            v_out_dim = qk_out_dim
+        if output_dim is None:
+            output_dim = v_out_dim
+
         self.num_heads = num_heads
-        self.head_dim = q_dim // num_heads
+        self.qk_head_dim = qk_out_dim // num_heads
+        self.v_head_dim = v_out_dim // num_heads
 
-        if head_dim is not None:
-            hidden_dim = head_dim * num_heads
-            self.head_dim = head_dim
-
-        self.k = nn.Linear(kv_dim, hidden_dim)
-        self.q = nn.Linear(q_dim, hidden_dim)
-        self.v = nn.Linear(kv_dim, hidden_dim)
-        self.projection = nn.Linear(hidden_dim, q_dim)
+        self.k = nn.Linear(kv_dim, qk_out_dim)
+        self.q = nn.Linear(q_dim, qk_out_dim)
+        self.v = nn.Linear(kv_dim, v_out_dim)
+        self.projection = nn.Linear(v_out_dim, output_dim)
         self.dropout = nn.Dropout(dropout)
-        self.scale = self.head_dim ** -0.5
+        self.scale = self.qk_head_dim ** -0.5
 
     def forward(
         self,
@@ -58,9 +68,9 @@ class MultiHeadAttention(nn.Module):
             Tensor of shape (B, N, D)
         """
         k, q, v = self.k(inputs_kv), self.q(inputs_q), self.v(inputs_kv)
-        k = rearrange(k, 'b s (n h) -> b n s h', h=self.head_dim)
-        q = rearrange(q, 'b s (n h) -> b n s h', h=self.head_dim)
-        v = rearrange(v, 'b s (n h) -> b n s h', h=self.head_dim)
+        k = rearrange(k, 'b s (n h) -> b n s h', h=self.qk_head_dim)
+        q = rearrange(q, 'b s (n h) -> b n s h', h=self.qk_head_dim)
+        v = rearrange(v, 'b s (n h) -> b n s h', h=self.v_head_dim)
         attention = (q @ k.transpose(-2, -1) / self.scale)
         if attention_mask is not None:
             min_value = torch.finfo(attention.dtype).min
@@ -107,9 +117,10 @@ class SelfAttention(nn.Module):
         self,
         *,
         hidden_dim: int,
+        qk_out_dim: Optional[int] = None,
+        v_out_dim: Optional[int] = None,
         widening_factor: int = 4,
         num_heads: int = 1,
-        head_dim: Optional[int] = None,
         dropout: float = 0.0,
         attention_dropout: float = 0.0
     ):
@@ -117,12 +128,13 @@ class SelfAttention(nn.Module):
 
         Args:
             hidden_dim: Dimension of input tensor.
+            qk_out_dim: Size of Query and Key matrices last dimension.
+                Defaults to None.
+            v_out_dim: Size of Value matrix last dimension.
+                Defaults to None.
             widening_factor: Feed-forward network widening factor.
                 Defaults to 4.
             num_heads: Number of attention heads. Defaults to 1.
-            head_dim: Size of single attention head. If None,this
-                value will be calculated as hidden_dim / num_heads.
-                Defaults to None.
             dropout: Dropout probability. Defaults to 0.
             attention_dropout: Attention scores probability. Defaults to 0.
         """
@@ -132,7 +144,9 @@ class SelfAttention(nn.Module):
         self.attention = MultiHeadAttention(
             kv_dim=hidden_dim,
             q_dim=hidden_dim,
-            head_dim=head_dim,
+            qk_out_dim=qk_out_dim,
+            v_out_dim=v_out_dim,
+            output_dim=hidden_dim,
             num_heads=num_heads,
             dropout=attention_dropout
         )
@@ -169,9 +183,10 @@ class CrossAttention(nn.Module):
         *,
         kv_dim: int,
         q_dim: int,
+        qk_out_dim: Optional[int] = None,
+        v_out_dim: Optional[int] = None,
         widening_factor: int = 1,
         num_heads: int = 1,
-        head_dim: Optional[int] = None,
         use_query_residual: bool = True,
         dropout: float = 0.0,
         attention_dropout: float = 0.0
@@ -181,12 +196,13 @@ class CrossAttention(nn.Module):
         Args:
             kv_dim: Dimension of key/value input tensor.
             q_dim: Dimension of query input tensor.
+            qk_out_dim: Size of Query and Key matrices last dimension.
+                Defaults to None.
+            v_out_dim: Size of Value matrix last dimension.
+                Defaults to None.
             widening_factor: Feed-forward network widening factor.
                 Defaults to 4.
             num_heads: Number of attention heads. Defaults to 1.
-            head_dim: Size of single attention head. If None,this
-                value will be calculated as hidden_dim / num_heads.
-                Defaults to None.
             use_query_residual: Indicates whether to use query residual in
                 cross-attention. Defaults to True.
             dropout: Dropout probability. Defaults to 0.
@@ -200,7 +216,9 @@ class CrossAttention(nn.Module):
         self.attention = MultiHeadAttention(
             kv_dim=kv_dim,
             q_dim=q_dim,
-            head_dim=head_dim,
+            qk_out_dim=qk_out_dim,
+            v_out_dim=v_out_dim,
+            output_dim=q_dim,
             num_heads=num_heads,
             dropout=attention_dropout
         )
